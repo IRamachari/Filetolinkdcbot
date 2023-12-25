02218322d89f2aab3730023f99428bb90f37e812
IRamachari <107060210+IRamachari@users.noreply.github.com>
2023-12-25 21:43:33 +0530

    Update __init__.py

--- Adarsh/bot/__init__.py
+++ Adarsh/bot/__init__.py
@@ -5,7 +5,7 @@ from ..vars import Var
 from os import getcwd
 
 StreamBot = Client(
-    session_name='Web Streamer',
+    session_name='Streamer',
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     bot_token=Var.BOT_TOKEN,
